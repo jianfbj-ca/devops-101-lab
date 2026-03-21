91c52f25d2a2511bf9f3b445cf93a3c57607d17f
jianfbj-ca <jian.feng.bj@gmail.com>
2026-03-20 22:07:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
   #获取系统环境信息，证明这是在Docker里运行
-  return "<h1>Success! The first Automated CI/CD Pipeline is ALIVE!</h1> |
+  return "<h1>Success! The first Automated CI/CD Pipeline is ALIVE!</h1> \
   <p>打通 GitHub -> AWS ECR -> VM2 自动化闭环！</p><p>Running through Docker Container and GitHub Actions!</p>" 
 
 if __name__ == "__main__":
