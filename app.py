6ed7338fddf8545b5b9cb38e089415b52d323477
jianfbj-ca <jian.feng.bj@gmail.com>
2026-03-20 22:53:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello():
   return "<h1>Success! The first Automated CI/CD Pipeline is ALIVE!</h1> \
   <p>打通 GitHub -> AWS ECR -> VM2 自动化闭环！</p><p>Running through Docker Container and GitHub Actions!</p>" 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
   #监听所有IP的5000端口
   app.run(host='0.0.0.0', port=5000)
